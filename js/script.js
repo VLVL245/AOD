e42fc14d2548cef9fdd2cdc4d573ee4c7f1ab339
Володя <chernyx.vova2@gmail.com>
2023-04-27 02:16:11 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,154 @@
+"use strict"
+
+// Меню burger =========================================================================================================
+
+window.onload = function () {
+	adaptiveHeader();
+}
+
+window.addEventListener('resize', function () {
+	adaptiveHeader();
+});
+
+function adaptiveHeader() {
+	let factWidth = window.innerWidth;
+	const headerBurger = document.querySelector('.header-menu__icon');
+	let headerMenuBurger = document.querySelector('.header-menu');
+	let menuBody = document.querySelector('.menu__body');
+	let menu = document.querySelector('.menu__list');
+	if (factWidth <= 767.98) {
+		if (!menu.classList.contains('done')) {
+			menu.classList.add('done');
+			[menu].forEach(element => {
+				headerMenuBurger.insertAdjacentElement('beforeend', element);
+			});
+		}
+	} else if (factWidth > 767.98) {
+		headerMenuBurger.classList.remove('_active');
+		headerBurger.classList.remove('_active');
+		if (menu.classList.contains('done')) {
+			menu.classList.remove('done');
+			[menu].forEach(element => {
+				menuBody.insertAdjacentElement('afterbegin', element);
+			});
+		}
+	}
+}
+
+const headerMenuList = document.querySelector('.menu__list');
+const headerMenuBurger = document.querySelector('.header-menu');
+const headerBurger = document.querySelector('.header-menu__icon');
+if (headerBurger) {
+	headerBurger.addEventListener("click", function (element) {
+		headerBurger.classList.toggle('_active');
+		headerMenuBurger.classList.toggle('_active');
+		if (headerMenuBurger.classList.contains('_active')) {
+			scrollStop();
+		} else {
+			scrollActive();
+		}
+	});
+	headerMenuList.addEventListener("click", function (element) {
+		headerBurger.classList.remove('_active');
+		headerMenuBurger.classList.remove('_active');
+		if (headerMenuBurger.classList.contains('_active')) {
+			scrollStop();
+		} else {
+			scrollActive();
+		}
+	});
+}
+
+function scrollStop() {
+	document.body.classList.add("stop-scrolling");
+}
+function scrollActive() {
+	document.body.classList.remove("stop-scrolling");
+}
+
+// Меню burger ======================================================================================================================
+
+// ibg cod=============================================================================================================
+
+function ibg() {
+	let ibg = document.querySelectorAll(".ibg");
+	for (var i = 0; i < ibg.length; i++) {
+		if (ibg[i].querySelector('img')) {
+			ibg[i].style.backgroundImage = 'url(\'' + ibg[i].querySelector('img').getAttribute('src') + '\')';
+		}
+	}
+}
+ibg();
+
+// ibg cod ================================================================================================================
+
+
+// активация swiper =================================================================================================================
+
+const swiper = new Swiper('.swiper', {
+	direction: 'horizontal',
+	loop: true,
+	spaceBetween: 10,
+	autoHeight: true,
+	pagination: {
+		el: '.swiper-pagination',
+	},
+});
+
+// активация swiper =========================================================================================
+
+// Валидация ввода email===================================================================================================
+
+
+const emailFild = document.getElementById('email');
+const button = document.getElementById('button');
+const response = document.getElementById('response');
+
+button.addEventListener('click', function () {
+	const email = emailFild.value;
+	if (validateEmail(email)) {
+		emailFild.classList.remove('_error');
+		response.innerHTML = "";
+	} else {
+		emailFild.classList.add('_error');
+		response.innerHTML = "Sorry, entered email incorrect";
+	}
+});
+
+function validateEmail(email) {
+	const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	return re.test(String(email).toLowerCase());
+}
+
+// Валидация ввода email =====================================================================================
+
+// Отправка формы на email===========================================================
+
+document.addEventListener('DOMContentLoaded', function () {
+	const form = document.getElementById('form');
+	let iconLoad = document.querySelector('.subscribe');
+	form.addEventListener('submit', formSend);
+
+	async function formSend(e) {
+		e.preventDefault();
+
+		let formData = new FormData(form);
+
+		iconLoad.classList.add('_sending');
+		let response = await fetch('sendmail.php', {
+			method: 'post',
+			body: formData
+		});
+		if (response.ok) {
+			let result = await response.json();
+			alert(result.message);
+			form.reset();
+			iconLoad.classList.remove('_sending');
+		} else {
+			alert("Ошибка");
+			iconLoad.classList.remove('_sending');
+
+		}
+
+	}
+});
