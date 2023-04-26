79531133a41821c75f270290fdc5520f325c3641
Володя <chernyx.vova2@gmail.com>
2023-04-27 02:20:18 +0300

    Add files via upload

---
+++ sendmail.php
@@ -0,0 +1,37 @@
+<?php
+	use PHPMailer\PHPMailer\PHPmailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	require 'phpmailer/src/Exception.php';
+	require 'phpmailer/src/PHPmailer.php';
+
+	$mail = new PHPMailer(true);
+	$mail->CharSet = 'UTF-8';
+	$mail->setLanguage('ru', 'phpmailer/language/');
+	$mail->IsHTML(true);
+
+	//От кого письмо
+	$mail->setFrom('chernyx.vova2@gmail.com');
+	//Кому отправить
+	$mail->addAddress('chernyx.vova2@gmail.com');
+	//Тема письма
+	$mail->Subject = 'Тест отпраки письма из формы';
+
+	if(trim(!empty($_POST['email']))){
+		$body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
+	}
+
+	$mail->Body = $body;
+
+	//Отправляем
+	if (!$mail->send()) {
+		$message = 'Ошибка';
+	} else {
+		$message = 'Данные отправлены!';
+	}
+
+	$response = ['message' => $message];
+
+	header('Content-type: application/json');
+	echo json_encode($response);
+?>
