7e5a2cb0ddbcac1ec95b8c8e61ca2966352b57c2
Володя <chernyx.vova2@gmail.com>
2023-05-11 22:35:18 +0300

    Update sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,38 +1,21 @@
 <?php
-	use PHPMailer\PHPMailer\PHPMailer;
-	use PHPMailer\PHPMailer\Exception;
-
-	require 'phpmailer/src/Exception.php';
-	require 'phpmailer/src/PHPMailer.php';
-        require 'path/to/PHPMailer/src/SMTP.php';
-
-	$mail = new PHPMailer(true);
-	$mail->CharSet = 'UTF-8';
-	$mail->setLanguage('ru', 'phpmailer/language/');
-	$mail->IsHTML(true);
-
-	//От кого письмо
-	$mail->setFrom('chernyx.vova2@gmail.com');
-	//Кому отправить
-	$mail->addAddress('chernyx.vova2@gmail.com');
-	//Тема письма
-	$mail->Subject = 'Тест отпраки письма из формы';
-
-	if(trim(!empty($_POST['email']))){
-		$body.='<p><strong>E-mail:</strong> '.$_POST['email'].'</p>';
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	function get_data() {
+		$datae = array();
+		$datae[] = array(
+			'Email' => $_POST['email'],
+		);
+		return json_encode($datae);
 	}
+	$name = "email";
+	$file_name = $name . '.json';
 
-	$mail->Body = $body;
-
-	//Отправляем
-	if (!$mail->send()) {
-		$message = 'Ошибка';
-	} else {
-		$message = 'Данные отправлены!';
-	}
-
-	$response = ['message' => $message];
-
-	header('Content-type: application/json');
-	echo json_encode($response);
+	if (file_put_contents(
+		"$file_name", get_dta())) {
+			echo $file_name . ' file created';
+		}
+		else {
+			echo 'There is some error';
+		}
+}
 ?>
